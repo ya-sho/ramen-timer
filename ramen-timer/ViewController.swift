7d9d4b64a26705489d0e9917de90b54cbd5062ff
ya-sho <78021007+ya-sho@users.noreply.github.com>
2021-03-16 10:57:22 +0900

    Initial Commit

---
+++ ramen-timer/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ramen-timer
+//
+//  Created by Shogo Yamamoto on 2021/03/16.
+//  Copyright © 2021年 Yamamoto Shogo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
